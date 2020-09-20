4e029cccfd80f7075b093cc0d00a3057b113db9b
ubuntunux <ubuntunux@gmail.com>
2020-09-20 17:07:17 +0900

    update

--- src/constants.rs
+++ src/constants.rs
@@ -1,95 +1,46 @@
-pub static ENGINE_NAME: &'static str = "RustEngine3D";
-pub static VULKAN_LAYERS: [&str; 1] = ["VK_LAYER_LUNARG_standard_validation"];
-pub static EXT_DEBUG_UTILS: bool = false;
-pub static KHR_SWAPCHAIN: bool = true;
+use vulkano::format::*;
 
-// depthFomats :: [VkFormat]
-// depthFomats = [VK_FORMAT_D32_SFLOAT, VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT, VK_FORMAT_D16_UNORM_S8_UINT, VK_FORMAT_D16_UNORM]
-//
-// depthStencilFormats :: [VkFormat]
-// depthStencilFormats = [VK_FORMAT_D32_SFLOAT_S8_UINT, VK_FORMAT_D24_UNORM_S8_UINT]
-//
-// invalidQueueIndex :: Word32
-// invalidQueueIndex = maxBound
-//
-// swapChainImageCount :: Int
-// swapChainImageCount = 3
-//
-// swapChainImageIndices :: [Int]
-// swapChainImageIndices = [0..(swapChainImageCount-1)]
-//
-// swapChainImageFormat :: VkFormat
-// swapChainImageFormat = VK_FORMAT_B8G8R8A8_SRGB
-//
-// maxFrameCount :: Int
-// maxFrameCount = 2
-//
-// descriptorSetCountAtOnce :: Int
-// descriptorSetCountAtOnce = swapChainImageCount
-//
-// maxDescriptorPoolAllocCount :: Int
-// maxDescriptorPoolAllocCount = 100
-//
-// enableImmediateMode :: Bool
-// enableImmediateMode = True
-//
-// enableValidationLayer :: Bool
-// enableValidationLayer = True
-//
-// isConcurrentMode :: Bool
-// isConcurrentMode = True
-//
-// meterPerUnit :: Float
-// meterPerUnit = 1.0::Float
-//
-// near :: Float
-// near = 0.1
-//
-// far :: Float
-// far = 2000.0
-//
-// fov :: Float
-// fov = 60.0
-//
-// cameraMoveSpeed :: Float
-// cameraMoveSpeed = 10.0
-//
-// cameraPanSpeed :: Float
-// cameraPanSpeed = 0.05
-//
-// cameraRotationSpeed :: Float
-// cameraRotationSpeed = 0.005
-//
-// shadowMapSize :: Word32
-// shadowMapSize = 2048
-//
-// shadowSamples :: Int
-// shadowSamples = 4
-//
-// shadowExp :: Float
-// shadowExp = 100.0
-//
-// shadowBias :: Float
-// shadowBias = 0.005
-//
-// shadowDistance :: Float
-// shadowDistance = 25.0
-//
-// shadowDepth :: Float
-// shadowDepth = 50.0
-//
-// shadowUpdateDistance :: Float
-// shadowUpdateDistance = 10.0
-//
-// _SSAO_KERNEL_SIZE :: Int
-// _SSAO_KERNEL_SIZE = 64
-//
-// _SSAO_RADIUS :: Float
-// _SSAO_RADIUS = 2.0
-//
-// _SSAO_NOISE_DIM :: Int
-// _SSAO_NOISE_DIM = 4
-//
+pub const ENGINE_NAME: &'static str = "RustEngine3D";
+pub const VULKAN_LAYERS: [&str; 1] = ["VK_LAYER_LUNARG_standard_validation"];
+pub const EXT_DEBUG_UTILS: bool = false;
+pub const KHR_SWAPCHAIN: bool = true;
+pub const DEPTH_FOMATS: [Format; 5] = [Format::D32Sfloat, Format::D32Sfloat_S8Uint, Format::D24Unorm_S8Uint, Format::D16Unorm_S8Uint, Format::D16Unorm];
+pub const DEPTH_STENCIL_FORMATS: [Format; 3] = [Format::D32Sfloat_S8Uint, Format::D24Unorm_S8Uint, Format::D16Unorm_S8Uint];
+pub const INVALID_QUEUE_INDEX: i32 = -1;
+pub const SWAP_CHAIN_IMAGE_COUNT: i32 = 3;
+pub const SWAP_CHAIN_IMAGE_INDICES: [i32; 3] = [0; SWAP_CHAIN_IMAGE_COUNT as usize];
+pub const SWAP_CHAIN_IMAGE_FORMAT:Format = Format::B8G8R8A8Srgb;
+pub const MAX_FRAME_COUNT: i32 = 2;
+pub const DESCRIPTOR_SET_COUNT_AT_ONCE: i32 = SWAP_CHAIN_IMAGE_COUNT;
+pub const MAX_DESCRIPTOR_POOL_ALLOC_COUNT: i32 = 100;
+pub const ENABLE_IMMEDIATE_MODE: bool = true;
+pub const ENABLE_VALIDATION_LAYER: bool = true;
+pub const IS_CONCURRENT_MODE: bool = true;
+pub const METER_PER_UNIT: f32 = 1.0;
+pub const NEAR: f32 = 0.1;
+pub const FAR: f32 = 2000.0;
+pub const FOV: f32 = 60.0;
+pub const CAMERA_MOVE_SPEED: f32 = 10.0;
+pub const CAMERA_PAN_SPEED: f32 = 0.05;
+pub const CAMERA_ROTATION_SPEED: f32 = 0.005;
+pub const SHADOW_MAP_SIZE: i32 = 2048;
+pub const SHADOW_SAMPLES: i32 = 4;
+pub const SHADOW_EXP: f32 = 100.0;
+pub const SHADOW_BIAS: f32 = 0.005;
+pub const SHADOW_DISTANCE: f32 = 25.0;
+pub const SHADOW_DEPTH: f32 = 50.0;
+pub const SHADOW_UPDATE_DISTANCE: f32 = 10.0;
+pub const SSAO_KERNEL_SIZE: i32 = 64;
+pub const SSAO_RADIUS: f32 = 2.0;
+pub const SSAO_NOISE_DIM: i32 = 4;
+
 // -- NOTE : sync with scene_constants.glsl
-// data RenderMode = RenderMode_Common | RenderMode_Shadow deriving (Eq, Enum, Ord, Show, Read)
-// data RenderObjectType = RenderObject_Static | RenderObject_Skeletal deriving (Eq, Enum, Ord, Show, Read)
+enum RenderMode {
+    RenderModeCommon,
+    RenderModeShadow
+}
+
+enum RenderObjectType {
+    RenderObjectStatic,
+    RenderObjectSkeletal
+}
