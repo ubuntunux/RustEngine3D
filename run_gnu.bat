536b7f7d7757e4863bd9ee3145abe632b4b2d533
ubuntunux <ubuntunux@gmail.com>
2020-09-23 10:00:50 +0900

    update

--- run_gnu.bat
+++ run_gnu.bat
@@ -1,1 +1,1 @@
-RUST_LOG=trace cargo run --target x86_64-pc-windows-gnu
+cargo run --target x86_64-pc-windows-gnu
--- run_msvc.bat
+++ run_msvc.bat
@@ -1,1 +1,1 @@
-RUST_LOG=trace cargo run --target x86_64-pc-windows-msvc
+cargo run --target x86_64-pc-windows-msvc
