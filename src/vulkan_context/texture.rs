a07b5e7db1cac0882325353ef0f73f725bc60533
ubuntu <ubuntunux@gmail.com>
2020-12-27 13:43:28 +0900

    update 2d array

--- src/vulkan_context/texture.rs
+++ src/vulkan_context/texture.rs
@@ -804,7 +804,7 @@ pub fn create_render_target<T>(
     };
     let (texture_create_flags, layer_count) = match texture_create_info._texture_view_type {
         vk::ImageViewType::CUBE => (vk::ImageCreateFlags::CUBE_COMPATIBLE, 6),
-        vk::ImageViewType::TYPE_2D_ARRAY => (vk::ImageCreateFlags::TYPE_2D_ARRAY_COMPATIBLE, texture_create_info._texture_layer),
+        vk::ImageViewType::TYPE_2D_ARRAY => (vk::ImageCreateFlags::empty(), texture_create_info._texture_layer),
         _ => (vk::ImageCreateFlags::empty(), 1),
     };
     let mip_levels = match texture_create_info._enable_mipmap {
@@ -925,7 +925,7 @@ pub fn create_texture_data<T: Copy>(
     };
     let (texture_create_flags, layer_count) = match texture_create_info._texture_view_type {
         vk::ImageViewType::CUBE => (vk::ImageCreateFlags::CUBE_COMPATIBLE, 6),
-        vk::ImageViewType::TYPE_2D_ARRAY => (vk::ImageCreateFlags::TYPE_2D_ARRAY_COMPATIBLE, texture_create_info._texture_layer),
+        vk::ImageViewType::TYPE_2D_ARRAY => (vk::ImageCreateFlags::empty(), texture_create_info._texture_layer),
         _ => (vk::ImageCreateFlags::empty(), 1),
     };
     let mip_levels = match texture_create_info._enable_mipmap {
