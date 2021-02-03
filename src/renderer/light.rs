298a3853d9c59ee5b564cad1fe84532e175387d7
ubuntunux <ubuntunux@gmail.com>
2021-02-04 02:29:18 +0900

    update shadow term

--- src/renderer/light.rs
+++ src/renderer/light.rs
@@ -1,8 +1,4 @@
-use nalgebra::{
-    Vector3,
-    Vector4,
-    Matrix4,
-};
+use nalgebra::{Vector3, Vector4, Matrix4, max};
 
 use crate::renderer::shader_buffer_datas::LightConstants;
 use crate::renderer::transform_object::{
@@ -13,6 +9,7 @@ use crate::utilities::math::{
     orthogonal,
 };
 use crate::constants;
+use cgmath::num_traits::real::Real;
 
 #[derive(Clone, Debug)]
 pub struct DirectionalLightCreateInfo {
@@ -20,6 +17,24 @@ pub struct DirectionalLightCreateInfo {
     pub _rotation: Vector3<f32>,
     pub _light_constants: LightConstants,
     pub _shadow_dimensions: Vector4<f32>,
+    pub _redraw_shadow_distance: f32,
+}
+
+impl Default for DirectionalLightCreateInfo {
+    fn default() -> DirectionalLightCreateInfo {
+        DirectionalLightCreateInfo {
+            _position: Vector3::zeros(),
+            _rotation: Vector3::new(std::f32::consts::PI * -0.5, 0.0, 0.0),
+            _light_constants: LightConstants::default(),
+            _redraw_shadow_distance: 5.0,
+            _shadow_dimensions: Vector4::new(
+                constants::SHADOW_DISTANCE,
+                constants::SHADOW_DISTANCE,
+                -constants::SHADOW_DEPTH,
+                constants::SHADOW_DEPTH
+            )
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -29,22 +44,8 @@ pub struct DirectionalLightData {
     pub _light_shadow_projection: Matrix4<f32>,
     pub _transform_object: TransformObjectData,
     pub _updated_light_data: bool,
-}
-
-impl Default for DirectionalLightCreateInfo {
-    fn default() -> DirectionalLightCreateInfo {
-        DirectionalLightCreateInfo {
-            _position: Vector3::zeros(),
-            _rotation: Vector3::new(std::f32::consts::PI * -0.5, 0.0, 0.0),
-            _light_constants: LightConstants::default(),
-            _shadow_dimensions: Vector4::new(
-                constants::SHADOW_DISTANCE,
-                constants::SHADOW_DISTANCE,
-                -constants::SHADOW_DEPTH,
-                constants::SHADOW_DEPTH
-            )
-        }
-    }
+    pub _need_to_redraw_shadow: bool,
+    pub _redraw_shadow_distance: f32,
 }
 
 impl DirectionalLightData {
@@ -56,6 +57,8 @@ impl DirectionalLightData {
             _light_shadow_projection: Matrix4::identity(),
             _transform_object: TransformObjectData::new_transform_object_data(),
             _updated_light_data: true,
+            _need_to_redraw_shadow: true,
+            _redraw_shadow_distance: light_create_info._redraw_shadow_distance,
         };
         light_data._transform_object.set_position(&light_create_info._position);
         light_data._transform_object.set_rotation(&light_create_info._rotation);
@@ -72,6 +75,11 @@ impl DirectionalLightData {
     pub fn get_light_shadow_exp(&self) -> f32 { self._light_constants._shadow_exp }
     pub fn get_light_shadow_bias(&self) -> f32 { self._light_constants._shadow_bias }
     pub fn get_shadow_view_projection(&self) -> &Matrix4<f32> { &self._light_constants._shadow_view_projection }
+    pub fn get_need_to_redraw_shadow_and_reset(&mut self) -> bool {
+        let need_to_redraw_shadow = self._need_to_redraw_shadow;
+        self._need_to_redraw_shadow = false;
+        need_to_redraw_shadow
+    }
 
     pub fn update_shadow_orthogonal(&mut self, shadow_dimensions: &Vector4<f32>) {
         let width = shadow_dimensions.x;
@@ -83,11 +91,16 @@ impl DirectionalLightData {
     }
 
     pub fn update_light_data(&mut self, view_position: &Vector3<f32>) {
-        self._transform_object.set_position(&view_position);
+        let delta: Vector3<f32> = (self._transform_object.get_position() - view_position).abs();
+        if self._redraw_shadow_distance < delta.max() {
+            self._transform_object.set_position(&view_position);
+        }
+
         let updated_transform = self._transform_object.update_transform_object();
         if self._updated_light_data || updated_transform {
             self._light_constants._shadow_view_projection = &self._light_shadow_projection * self._transform_object.get_inverse_matrix();
             self._light_constants._light_direction = self.get_light_direction().clone() as Vector3<f32>;
+            self._need_to_redraw_shadow = true;
         }
         self._updated_light_data = false;
     }
--- src/renderer/renderer.rs
+++ src/renderer/renderer.rs
@@ -983,8 +983,10 @@ impl RendererData {
                 let command_buffer = self._command_buffers[swapchain_index as usize];
                 let resources = self._resources.borrow();
                 let main_camera =  scene_manager.get_main_camera().borrow();
-                let main_light = scene_manager.get_main_light().borrow();
-                let capture_height_map = scene_manager.get_capture_height_map().borrow();
+                let mut main_light = scene_manager.get_main_light().borrow_mut();
+                let render_main_light_shadow = main_light.get_need_to_redraw_shadow_and_reset();
+                let mut capture_height_map = scene_manager.get_capture_height_map().borrow_mut();
+                let render_capture_height_map: bool = capture_height_map.get_need_to_redraw_shadow_and_reset();
                 let fft_ocean =  scene_manager.get_fft_ocean().borrow();
                 let mut atmosphere =  scene_manager.get_atmosphere().borrow_mut();
                 let quad_mesh = resources.get_mesh_data("quad").borrow();
@@ -1008,7 +1010,10 @@ impl RendererData {
                     fft_ocean.get_height(),
                 );
                 self._view_constants.update_view_constants(&main_camera);
-                self._view_constants._capture_height_map_view_projection = capture_height_map._light_constants._shadow_view_projection.into();
+                if render_capture_height_map {
+                    log::info!("render_capture_height_map");
+                    self._view_constants._capture_height_map_view_projection = capture_height_map._light_constants._shadow_view_projection.into();
+                }
 
                 self.upload_shader_buffer_data(swapchain_index, ShaderBufferDataType::SceneConstants, &self._scene_constants);
                 self.upload_shader_buffer_data(swapchain_index, ShaderBufferDataType::ViewConstants, &self._view_constants);
@@ -1022,17 +1027,21 @@ impl RendererData {
                     atmosphere.precompute(command_buffer, swapchain_index, &quad_geometry_data, self);
                 }
 
-                // clear
+                // clear gbuffer
                 self.render_material_instance(command_buffer, swapchain_index, "clear_framebuffer", "clear_gbuffer/clear", &quad_geometry_data, None, None, NONE_PUSH_CONSTANT);
-                self.render_material_instance(command_buffer, swapchain_index, "clear_framebuffer", "clear_shadow/clear", &quad_geometry_data, None, None, NONE_PUSH_CONSTANT);
-                self.render_material_instance(command_buffer, swapchain_index, "clear_framebuffer", "clear_capture_height_map/clear", &quad_geometry_data, None, None, NONE_PUSH_CONSTANT);
 
                 // shadow
-                self.render_solid_object(command_buffer, swapchain_index, RenderMode::Shadow, RenderObjectType::Static, &static_render_elements, None);
-                self.render_solid_object(command_buffer, swapchain_index, RenderMode::Shadow, RenderObjectType::Skeletal, &skeletal_render_elements, None);
+                if render_main_light_shadow {
+                    self.render_material_instance(command_buffer, swapchain_index, "clear_framebuffer", "clear_shadow/clear", &quad_geometry_data, None, None, NONE_PUSH_CONSTANT);
+                    self.render_solid_object(command_buffer, swapchain_index, RenderMode::Shadow, RenderObjectType::Static, &static_render_elements, None);
+                    self.render_solid_object(command_buffer, swapchain_index, RenderMode::Shadow, RenderObjectType::Skeletal, &skeletal_render_elements, None);
+                }
 
                 // capture height map
-                self.render_solid_object(command_buffer, swapchain_index, RenderMode::CaptureHeightMap, RenderObjectType::Static, &static_render_elements, None);
+                if render_capture_height_map {
+                    self.render_material_instance(command_buffer, swapchain_index, "clear_framebuffer", "clear_capture_height_map/clear", &quad_geometry_data, None, None, NONE_PUSH_CONSTANT);
+                    self.render_solid_object(command_buffer, swapchain_index, RenderMode::CaptureHeightMap, RenderObjectType::Static, &static_render_elements, None);
+                }
 
                 // fft-simulation
                 fft_ocean.simulate_fft_waves(command_buffer, swapchain_index, &quad_geometry_data, self, &resources);
