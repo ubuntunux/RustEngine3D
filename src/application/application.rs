2aad8925bc6b83802691bc3177af8c896524db11
ubuntunux <ubuntunux@gmail.com>
2021-02-16 03:01:21 +0900

    update android

--- src/application/application.rs
+++ src/application/application.rs
@@ -262,6 +262,22 @@ pub fn run_application() {
     let mut run_application: bool = false;
     event_loop.run(move |event, __window_target, control_flow|{
         if need_initialize {
+            if maybe_application_data.is_some() {
+                // clear up
+                let mut application_data: RefMut<ApplicationData> = maybe_application_data.as_ref().unwrap().borrow_mut();
+                let mut renderer_data: RefMut<RendererData> = maybe_renderer_data.as_ref().unwrap().borrow_mut();
+                let mut scene_manager_data: RefMut<SceneManagerData> = maybe_scene_manager_data.as_ref().unwrap().borrow_mut();
+                let mut font_manager: RefMut<FontManager> = maybe_font_manager.as_ref().unwrap().borrow_mut();
+                let mut ui_manager: RefMut<UIManager> = maybe_ui_manager.as_ref().unwrap().borrow_mut();
+                application_data.terminate_applicateion(
+                    &mut font_manager,
+                    &mut ui_manager,
+                    &mut scene_manager_data,
+                    &mut maybe_resources.as_ref().unwrap().borrow_mut(),
+                    &mut renderer_data,
+                );
+            }
+
             let elapsed_time = time_instance.elapsed().as_secs_f64();
             let (width, height) = window_size;
             let mouse_pos = (width / 2, height / 2);
@@ -328,30 +344,7 @@ pub fn run_application() {
             Event::Suspended => {
                 log::info!("Application was suspended");
                 #[cfg(target_os = "android")]
-                if run_application {
-                    {
-                        // Destroy app on suspend for android target.
-                        let mut application_data: RefMut<ApplicationData> = maybe_application_data.as_ref().unwrap().borrow_mut();
-                        let mut renderer_data: RefMut<RendererData> = maybe_renderer_data.as_ref().unwrap().borrow_mut();
-                        let mut scene_manager_data: RefMut<SceneManagerData> = maybe_scene_manager_data.as_ref().unwrap().borrow_mut();
-                        let mut font_manager: RefMut<FontManager> = maybe_font_manager.as_ref().unwrap().borrow_mut();
-                        let mut ui_manager: RefMut<UIManager> = maybe_ui_manager.as_ref().unwrap().borrow_mut();
-                        application_data.terminate_applicateion(
-                            &mut font_manager,
-                            &mut ui_manager,
-                            &mut scene_manager_data,
-                            &mut maybe_resources.as_ref().unwrap().borrow_mut(),
-                            &mut renderer_data,
-                        );
-                    }
-
-                    maybe_resources = None;
-                    maybe_ui_manager = None;
-                    maybe_font_manager = None;
-                    maybe_renderer_data = None;
-                    maybe_scene_manager_data = None;
-                    maybe_application_data = None;
-
+                if initialize_done {
                     run_application = false;
                     initialize_done = false;
                 }
@@ -396,31 +389,40 @@ pub fn run_application() {
 
                     font_manager.log(format!("{:.2}fps / {:.3}ms", application_data._time_data._average_fps, application_data._time_data._average_frame_time));
 
-                    // update && render
                     if renderer_data.get_need_recreate_swapchain() {
-                        log::info!("<<begin recreate_swapchain>>");
+                        #[cfg(target_os = "android")]
+                        {
+                            // Destroy app on suspend for android target.
+                            run_application = false;
+                            initialize_done = false;
+                        }
+                        #[cfg(not(target_os = "android"))]
+                        {
+                            log::info!("<<begin recreate_swapchain>>");
 
-                        // destroy
-                        scene_manager_data.destroy_scene_graphics_data(renderer_data.get_device());
-                        ui_manager.destroy_ui_descriptor_sets();
-                        font_manager.destroy_font_descriptor_sets();
+                            // destroy
+                            scene_manager_data.destroy_scene_graphics_data(renderer_data.get_device());
+                            ui_manager.destroy_ui_descriptor_sets();
+                            font_manager.destroy_font_descriptor_sets();
 
-                        renderer_data.resize_window();
+                            renderer_data.resize_window();
 
-                        // recreate
-                        font_manager.create_font_descriptor_sets(&renderer_data, &renderer_data._resources.borrow());
-                        ui_manager.create_ui_descriptor_sets(&renderer_data, &renderer_data._resources.borrow());
-                        scene_manager_data.initialize_scene_graphics_data(&renderer_data);
-                        renderer_data.set_need_recreate_swapchain(false);
+                            // recreate
+                            font_manager.create_font_descriptor_sets(&renderer_data, &renderer_data._resources.borrow());
+                            ui_manager.create_ui_descriptor_sets(&renderer_data, &renderer_data._resources.borrow());
+                            scene_manager_data.initialize_scene_graphics_data(&renderer_data);
+                            renderer_data.set_need_recreate_swapchain(false);
 
-                        log::info!("<<end recreate_swapchain>>");
+                            log::info!("<<end recreate_swapchain>>");
+                        }
+                    } else {
+                        // update && render
+                        renderer_data.update_post_process_datas();
+                        scene_manager_data.update_scene_manager_data(elapsed_time, delta_time);
+                        font_manager.update();
+                        ui_manager.update(delta_time);
+                        renderer_data.render_scene(scene_manager_data, &mut font_manager, &mut ui_manager, elapsed_time, delta_time, elapsed_frame);
                     }
-
-                    renderer_data.update_post_process_datas();
-                    scene_manager_data.update_scene_manager_data(elapsed_time, delta_time);
-                    font_manager.update();
-                    ui_manager.update(delta_time);
-                    renderer_data.render_scene(scene_manager_data, &mut font_manager, &mut ui_manager, elapsed_time, delta_time, elapsed_frame);
                 }
             },
             Event::WindowEvent { event, .. } => match event {
