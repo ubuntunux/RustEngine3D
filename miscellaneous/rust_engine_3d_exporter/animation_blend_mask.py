0355bf6c722e1e17045bffd9965ed1def26f57ac
ubuntunux <ubuntunux@gmail.com>
2024-06-12 01:59:13 +0900

    update animation blend mask

---
+++ miscellaneous/rust_engine_3d_exporter/animation_blend_mask.py
@@ -0,0 +1,16 @@
+import bpy
+
+def create_animation_blend_mask(armature):
+    if armature.type != 'ARMATURE':
+        return
+    
+    bpy.ops.object.constraint_add(type='ARMATURE')
+    for bone in armature.data.bones:
+        target = armature.constraints["Armature"].targets.new()
+        target.target = armature.override_library.reference
+        target.subtarget = bone.name
+        
+        
+
+
+create_animation_blend_mask(bpy.context.object)
